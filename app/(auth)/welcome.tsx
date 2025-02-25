2383b9d5b063cc15b4cbf25e02ee52fd2de42b7d
fred42vid <fred42vid@gmail.com>
2025-02-25 12:00:51 +0100

    Swiper with 3 onboarding images

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,8 +1,14 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from 'react';
+import { onboarding } from '@/constants';
 
 const Welcome = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -13,6 +19,25 @@ const Welcome = () => {
       >
         <Text className='text-black text-md font-JakartaBold'>Skip</Text>
       </TouchableOpacity >
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className='w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full' />}
+        activeDot={<View className='w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full' />}
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className='flex items-center justify-center p-5'>
+            <Image
+              source={item.image}
+              className='w-full h-[300px]'
+              resizeMode="contain"
+            />
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView >
   );
 };
