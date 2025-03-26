2fe5da5ffef1b04afffb74a9bfd7dba0c07d1da0
fred42vid <fred42vid@gmail.com>
2025-03-26 12:25:32 +0100

    moved some SignedIn/SignedOut boilerplate from home to profile, because it might be more useful here

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,50 @@
-import { Text } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+import { icons } from '@/constants';
+import { SignedIn, SignedOut, useAuth, useUser } from '@clerk/clerk-expo'
+import { Link, router } from 'expo-router'
+import { Image, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Profile = () => {
+  const { user } = useUser()
+  const { signOut } = useAuth();
+
+  const handleSignOut = () => {
+    signOut();
+    router.replace("/(auth)/log-in");
+  };
+
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <Text>Profile</Text>
+      <SignedIn>
+        <Text>Hi {user?.emailAddresses[0].emailAddress}</Text>
+        <CustomButton
+          title="Ausloggen"
+          className="mt-5 w-full shadow-none pb-100"
+          IconLeft={() => (
+            <Image
+              source={icons.out}
+              resizeMode="contain"
+              className="w-5 h-5 mx-2"
+            />
+          )}
+          bgVariant="outline"
+          textVariant="primary"
+          onPress={handleSignOut}
+        />
+        {/* CustomButton: pb-100 is useless, since there is
+            no scrolling possible as of now */}
+        <View className='mb-10' />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/log-in">
+          <Text>Log in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
     </SafeAreaView>
   );
 };
